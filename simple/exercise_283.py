950dba9ff8b71582bf1f4a2d5a5f1be818d092ea
chenzhiqun <chenzhiqun@guanmai.cn>
2023-01-30 10:32:18 +0800

    题283：移动0

---
+++ simple/exercise_283.py
@@ -0,0 +1,24 @@
+"""
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+"""
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow, fast = 0, 0
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
+        return nums
+
+
+solution = Solution()
+print(solution.moveZeroes([0, 1, 0, 3, 12]))
