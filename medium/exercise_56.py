a7fbe7b809f27fa25159a930359055094d52950a
chenzhiqun <chenzhiqun@guanmai.cn>
2023-02-13 18:27:32 +0800

    题56：螺旋矩阵

---
+++ medium/exercise_56.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        """保持循环变量不变（左闭右开）"""
+        nums = [[0] * n for _ in range(n)]
+        startx, starty = 0, 0  # 起始点
+        loop, mid = n // 2, n // 2  # 迭代次数、n为奇数时，矩阵的中心点
+        count = 1  # 计数
+
+        for offset in range(1, loop + 1):  # 每循环一层偏移量加1，偏移量从1开始
+            # 按 "从左到右-> 从上到下 -> 从右到左 -> 从下到上 到顺序依次填充"
+            for i in range(starty, n - offset):  # 从左至右，左闭右开
+                nums[startx][i] = count
+                count += 1
+            for i in range(startx, n - offset):  # 从上至下
+                nums[i][n - offset] = count
+                count += 1
+            for i in range(n - offset, starty, -1):  # 从右至左
+                nums[n - offset][i] = count
+                count += 1
+            for i in range(n - offset, startx, -1):  # 从下至上
+                nums[i][starty] = count
+                count += 1
+            startx += 1  # 更新起始点
+            starty += 1
+
+        if n % 2 != 0:  # n为奇数时，填充中心点
+            nums[mid][mid] = count
+        return nums
+
+
+solution = Solution()
+print(solution.generateMatrix(3))
+print(solution.generateMatrix(4))
