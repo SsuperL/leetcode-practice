3f828a50c1b5ad793686e8bbf347032f406d0347
chenzhiqun <chenzhiqun@guanmai.cn>
2023-03-27 17:47:18 +0800

    题844：增加快慢指针方法

--- simple/exercise_844.py
+++ simple/exercise_844.py
@@ -11,25 +11,50 @@
 
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        res1 = ""
-        res2 = ""
-        for i in s:
-            if i != "#":
-                res1 += i
-            else:
-                res1 = res1[:-1]
+        # res1 = ""
+        # res2 = ""
+        # for i in s:
+        #     if i != "#":
+        #         res1 += i
+        #     else:
+        #         res1 = res1[:-1]
+        #
+        # for j in t:
+        #     if j != "#":
+        #         res2 += j
+        #     else:
+        #         res2 = res2[:-1]
+        #
+        # if res1 == res2:
+        #     return True
+        # return False
 
-        for j in t:
-            if j != "#":
-                res2 += j
-            else:
-                res2 = res2[:-1]
+        # 方法二：栈
+        # 方法三：快慢指针
+        def back(l):
+            slow, fast = 0, 0
+            while fast < len(l):
+                if l[fast] == "#":
+                    if slow > 0:
+                        slow -= 1
+                    fast += 1
+                    continue
+                l[slow] = l[fast]
+                slow += 1
+                fast += 1
+            return slow
 
-        if res1 == res2:
-            return True
-        return False
+        data_1, data_2 = list(s), list(t)
+        slow_1 = back(data_1)
+        slow_2 = back(data_2)
+        print(data_1, data_2)
+        print(slow_1, slow_2)
+        return data_1[:slow_1] == data_2[:slow_2]
 
 
 solution = Solution()
-s, t = "xywrrmp", "xywrrm#p"
+# s, t = "xywrrmp", "xywrrm#p"
+# print(solution.backspaceCompare(s, t))
+s = "y#fo##f"
+t = "y#f#o##f"
 print(solution.backspaceCompare(s, t))
