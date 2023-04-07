569d2cd95c075599cbc0b4e331daf94017e8a319
chenzhiqun <chenzhiqun@guanmai.cn>
2023-04-07 16:54:21 +0800

    题144：二叉树前序遍历（迭代法）

--- simple/exercise_144.py
+++ simple/exercise_144.py
@@ -32,3 +32,23 @@ class Solution:
 
         traverse(root)
         return res
+
+    def preorderTraversal_2(self, root: Optional[TreeNode]) -> List[int]:
+        """前序遍历：迭代法"""
+        # 使用栈实现
+        # 节点入栈顺序为： 中节点-> 右节点 -> 左节点
+        res = []
+        if not root:
+            return []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if not node:
+                return res
+            res.append(node.val)
+            # 先让右节点先于左节点入栈，保证出栈顺序是中->左->右
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
