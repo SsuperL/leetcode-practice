f117fe0f1d8fb678c755b04fa9c326a50db4252c
chenzhiqun <chenzhiqun@guanmai.cn>
2023-04-19 18:12:14 +0800

    题116：填充每个节点的下一个右侧节点指针

---
+++ medium/exercise_116.py
@@ -0,0 +1,36 @@
+"""
+给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+"""
+from collections import deque
+from typing import Optional
+
+"""
+# Definition for a Node.
+"""
+
+
+class Node:
+    def __init__(self, val: int = 0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: Optional[Node]) -> Optional[Node]:
+        q = deque()
+        if not root:
+            return
+        q.append(root)
+        while q:
+            size = len(q)
+            for i in range(size):
+                node = q.popleft()
+                if i < size - 1:
+                    node.next = q[0]
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return root
