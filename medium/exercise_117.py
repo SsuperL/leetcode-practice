6deb745f096c8c8d4f8a538143d697d105c9d1ec
chenzhiqun <chenzhiqun@guanmai.cn>
2023-04-19 18:20:12 +0800

    题116：填充每个节点的下一个右侧节点指针II

---
+++ medium/exercise_117.py
@@ -0,0 +1,45 @@
+"""
+给定一个二叉树：
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from collections import deque
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root):
+        q = deque()
+        if not root:
+            return
+        q.append(root)
+        while q:
+            size = len(q)
+            for i in range(size):
+                node = q.popleft()
+                if i < size - 1:
+                    node.next = q[0]
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return root
