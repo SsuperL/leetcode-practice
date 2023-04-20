dc92654daf8eaee084fae27ac79272be25023878
chenzhiqun <chenzhiqun@guanmai.cn>
2023-04-20 16:56:33 +0800

    题104：二叉树最大深度

---
+++ simple/exercise_104.py
@@ -0,0 +1,35 @@
+"""
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+"""
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        q = deque()
+        if not root:
+            return res
+        q.append(root)
+        while q:
+            res += 1
+            print(res)
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        return res
